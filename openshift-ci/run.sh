af8be5b19c45810b7765173b450794d490ce902b
Jakub Jedlicka <jjedlick@redhat.com>
2024-08-30 09:41:59 +0200

    Add comment that this file is not used while testing

--- openshift-ci/run.sh
+++ openshift-ci/run.sh
@@ -3,6 +3,8 @@ set -eux
 
 export _JAVA_OPTIONS=-Duser.home=$HOME
 
+# The CI testing not using this file, to modify the mvn command
+# change this file https://github.com/openshift/release/blob/master/ci-operator/step-registry/quarkus/execute-tests/quarkus-execute-tests-commands.sh
 mvn -B -V clean verify -fae \
     -Dmaven.repo.local=$PWD/local-repo \
     -Dquarkus.platform.group-id=$QUARKUS_PLATFORM_GROUP_ID \
